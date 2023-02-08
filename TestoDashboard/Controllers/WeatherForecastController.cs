c143a96245678ec26dd73f80275b2b5a9b724f50
Muayyad <kaka-mah@hotmail.com>
2023-02-08 04:42:57 +0300

    testing asp

--- TestoDashboard/Controllers/WeatherForecastController.cs
+++ TestoDashboard/Controllers/WeatherForecastController.cs
@@ -19,15 +19,19 @@ namespace TestoDashboard.Controllers
         }
 
         [HttpGet(Name = "GetWeatherForecast")]
-        public IEnumerable<WeatherForecast> Get()
+        public IEnumerable<int> Get()
         {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            IEnumerable<int> result = from value in Enumerable.Range(0, 5)
+                                      select value;
+            return result.ToArray();
+            /* return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateTime.Now.AddDays(index),
                 TemperatureC = Random.Shared.Next(-20, 55),
                 Summary = Summaries[Random.Shared.Next(Summaries.Length)]
             })
-            .ToArray();
+            .ToArray(); */
         }
+
     }
 }
